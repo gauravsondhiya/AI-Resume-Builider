e8c6adc173b941944c8f605475fbfa95cbffbcd5
gauravsondhiya <gauravsondhiya1503@gmail.com>
2025-05-27 16:19:52 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+  let [data,setdata] = useState('')
+  let [output,setoutput]= useState()
+  useEffect(()=>{
+fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyA4TX_kFRDn-8tEOlW4vdQ67wZAHhPfW-I, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    contents: [{ parts: [{ text: "who is  actress" }] }]
+  })
+})
+.then(res => res.json())
+.then(data => setdata(data.candidates[0].content.parts[0].text))})
+
+
+
   return (
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div>
+      <h1>{data}</h1>
+    </div>
   )
 }
 
 export default App
+
+
+
+
